0b6ab5bfa675482728b11207267419ea839a4b71
wihtoritorkkeli <awito001@edu.xamk.fi>
2023-11-18 19:33:06 +0200

    käyttäjien lisäs, muokkaus ja poisto onnistuu

--- routes/apiKayttajat.ts
+++ routes/apiKayttajat.ts
@@ -32,9 +32,69 @@ apiKayttajaRouter.delete("/:id",async(req: express.Request, res: express.Respons
 });
 
 //Muokataan käyttäjän tietoja tietokannassa
+apiKayttajaRouter.put("/:id", async(req: express.Request, res: express.Response, next: express.NextFunction)=>{
+    try{
+        if(await prisma.kayttaja.count({
+            where: {
+                id: Number(req.params.id)
+            }
+        })===1){
+            if((req.body.kayttajatunnus.length > 0 && req.body.kayttajatunnus !== undefined)&&
+            (req.body.salasana.length > 0 && req.body.salasana !== undefined)){
+                try{
+                    await prisma.kayttaja.update({
+                        where:{
+                            id: Number(req.params.id)
+                        },
+                        data : {
+                            kayttajatunnus : req.body.kayttajatunnus,
+                            salasana : req.body.salasana
+                        }
+                    });
+                    res.status(200).json(await prisma.kayttaja.findUnique({where:{id: Number(req.params.id)}}));
+                }
+                catch(e:any){
+                    next(new Virhe());
+                }
+            }
+            else{
+                next(new Virhe(400, "Tiedot puutteellisia"));
+            }
+        }
+        else{
+            next(new Virhe(404, "Not found"));
+        }
+    }
+    catch(e:any){
+        next(new Virhe());
+    }
+});
 
 
 //Lisätään käyttäjä tietokantaan
+apiKayttajaRouter.post("/", async(req: express.Request, res: express.Response, next: express.NextFunction)=>{
+    try{
+        if((req.body.kayttajatunnus.length > 0 && req.body.kayttajatunnus !== undefined)&&
+        (req.body.salasana.length > 0 && req.body.salasana !== undefined)
+        ){
+            try{
+                await prisma.kayttaja.create({
+                    data: {
+                        kayttajatunnus : req.body.kayttajatunnus,
+                        salasana : req.body.salasana
+                    }
+                });
+                res.status(200).json(await prisma.kayttaja.findMany({orderBy:{id: "desc"}, take: 1}));
+            }
+            catch(e:any){
+                next(new Virhe());
+            }
+        }
+    }
+    catch(e:any){
+        next(new Virhe());
+    }
+});
 
 
 //Haetaan käyttäjä ID:n perusteella tietokannasta
