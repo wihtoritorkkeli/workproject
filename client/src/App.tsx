c0ed6961d702143e5d5f2475f6b1bf44173e3427
wihtoritorkkeli <awito001@edu.xamk.fi>
2023-11-18 20:57:00 +0200

    Front end pohja luotu

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+import './App.css';
+
+const App : React.FC = () : React.ReactElement => {
+  
+  return (
+    <Container>
+      <Typography variant='h1'>Hello World!</Typography>
+    </Container>
+  );
+}
+
+export default App;
