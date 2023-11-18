f2d1fa33f02d4b6ec694519b3dc776bd3ee67a8d
wihtoritorkkeli <awito001@edu.xamk.fi>
2023-11-18 21:28:38 +0200

    Front end etenee vaikkakin hitaasti

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,74 @@
-import React from 'react';
-import { Container, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Container, Typography, Alert, Backdrop, CircularProgress, Stack, ListItem, ListItemText, List } from '@mui/material';
 import './App.css';
 
+interface Kayttaja {
+  id : number
+  kayttajatunnus : string
+  salasana : string
+  admin : boolean
+}
+
+interface apiData {
+  kayttajat : Kayttaja[]
+  virhe : string
+  haettu : boolean
+}
+
 const App : React.FC = () : React.ReactElement => {
-  
+
+  const [apiData, setApidata] = useState<apiData>({
+    kayttajat : [],
+    virhe: "",
+    haettu : false
+  });
+
+
+  const apiKutsu = async () : Promise<void> => {
+    try{
+      
+      const kayttajaYhteys = await fetch('http://localhost:3004/api/kayttajat');
+
+      setApidata({
+        ...apiData,
+        kayttajat: await kayttajaYhteys.json(),
+        haettu : true
+      })
+
+    }
+    catch(e:any){
+
+      setApidata({
+        ...apiData,
+        virhe : "palvelimeen ei saada yhteyttä"
+      })
+
+    }
+  };
+
+  useEffect(() =>{
+    apiKutsu();
+  }, []);
+
   return (
     <Container>
       <Typography variant='h1'>Hello World!</Typography>
+      <Typography variant='h6'>Käyttäjät:</Typography>
+
+      {(Boolean(apiData.virhe))
+        ? <Alert severity='error'>apiData.virhe</Alert>
+        : (apiData.haettu)
+          ? <Stack>
+              <List>
+                {apiData.kayttajat.map((Kayttaja : Kayttaja) => {
+                  return  <ListItem key={Kayttaja.id}>
+                            <ListItemText primary={Kayttaja.kayttajatunnus}/>
+                          </ListItem>
+                })}
+              </List>
+          </Stack>
+          : <Backdrop open={true}><CircularProgress color='inherit'/></Backdrop>
+      }
     </Container>
   );
 }
--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import virheenkasittelija from './virheenkasittelija';
 import apiKayttajaRouter from './routes/apiKayttajat';
 import apiLiidiRouter from './routes/apiLiidit';
 
 const app : express.Application = express();
 const port : number = Number(process.env.PORT) || 3004;
+
+app.use(cors({origin: "http://localhost:3000"}));
 
 app.use(express.static(path.resolve(__dirname, "public")));
 
