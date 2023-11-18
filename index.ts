633fdac8d6fe46cad9145ccf2fb1e20887135d23
wihtoritorkkeli <awito001@edu.xamk.fi>
2023-11-18 18:54:59 +0200

    backend etenee

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import path from 'path';
 import virheenkasittelija from './virheenkasittelija';
+import apiKayttajaRouter from './routes/apiKayttajat';
 
 const app : express.Application = express();
 const port : number = Number(process.env.PORT) || 3004;
 
 app.use(express.static(path.resolve(__dirname, "public")));
+
+app.use("/api/kayttajat", apiKayttajaRouter);
 
 app.use(virheenkasittelija);
 
--- routes/apiKayttajat.ts
+++ routes/apiKayttajat.ts
@@ -1,2 +1,70 @@
 import express from "express";
+import { Virhe } from "../virheenkasittelija";
+import { PrismaClient } from "@prisma/client";
 
+const apiKayttajaRouter : express.Router = express.Router();
+const prisma : PrismaClient = new PrismaClient();
+
+apiKayttajaRouter.use(express.json());
+
+//Poistetaan käyttäjä tietokannasta
+apiKayttajaRouter.delete("/:id",async(req: express.Request, res: express.Response, next: express.NextFunction) => {
+    if(await prisma.kayttaja.count({
+        where: {
+            id: Number(req.params.id)
+        }
+    })=== 1){
+        try{
+            res.status(200).json(await prisma.kayttaja.findUnique({where:{id: Number(req.params.id)}}));
+            await prisma.kayttaja.delete({
+                where: {
+                    id: Number(req.params.id)
+                }
+            });
+        }
+        catch(e:any){
+            next(new Virhe());
+        }
+    }
+    else{
+        next(new Virhe(404, "Not found"));
+    }
+});
+
+//Muokataan käyttäjän tietoja tietokannassa
+
+
+//Lisätään käyttäjä tietokantaan
+
+
+//Haetaan käyttäjä ID:n perusteella tietokannasta
+apiKayttajaRouter.get("/:id", async(req: express.Request, res: express.Response, next: express.NextFunction) => {
+    if(await prisma.kayttaja.count({
+        where: {
+            id: Number(req.params.id)
+        }
+    })===1){
+        try{
+            res.status(200).json(await prisma.kayttaja.findUnique({where:{id: Number(req.params.id)}}));
+            res.json(await prisma.kayttaja.findUnique({where:{id: Number(req.params.id)}}));
+        }
+        catch(e:any){
+            next(new Virhe());
+        }
+    }else{
+        next(new Virhe(404, "Not Found"));
+    }
+});
+
+//Haetaan kaikki käyttäjät
+apiKayttajaRouter.get("/", async(req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try{
+        res.json(await prisma.kayttaja.findMany());
+    }
+    catch(e:any){
+        next(new Virhe());
+    }
+});
+
+
+export default apiKayttajaRouter;
